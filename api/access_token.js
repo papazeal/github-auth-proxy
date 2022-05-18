dc7460fc012fa75b30edad952fddc525123ee2dd
Nutrawee <zeally7@gmail.com>
2022-05-18 14:46:57 +0700

    add option to use 'state' for dynamic redirect

--- api/access_token.js
+++ api/access_token.js
@@ -1,7 +1,14 @@
 import fetch from 'node-fetch'
 
 export default async function handler(request, response) {
-  const { code } = request.query
+  const { code, state } = request.query
+  const isUrl = (string) => {
+    try {
+      return Boolean(new URL(string))
+    } catch (e) {
+      return false
+    }
+  }
 
   const authResponse = await fetch(
     'https://github.com/login/oauth/access_token?code=' +
@@ -17,11 +24,18 @@ export default async function handler(request, response) {
   const authResponseText = await authResponse.text()
 
   if (process.env.REDIRECT_URI) {
+    // redirect to REDIRECT_URI
     response
       .writeHead(301, {
         Location: process.env.REDIRECT_URI + '?' + authResponseText,
       })
       .end()
+  } else if (state && isUrl(state)) {
+    // if REDIRECT_URI is not defined
+    // use state params for dynamic redirect
+    response.writeHead(301, {
+      Location: state + '?' + authResponseText,
+    })
   } else {
     response.status(200).json({
       Response: authResponseText,
